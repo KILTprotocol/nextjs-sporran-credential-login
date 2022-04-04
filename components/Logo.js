b84fbd53e87bc189c3218b7b1bc4f2d41aacf8d2
paulpomerleau <paul.m.pomerleau@gmail.com>
2022-04-04 12:00:37 -0400

    change logo title

--- components/Logo.js
+++ components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className={styles.container}>
       <img src="/kilt-logo.svg" className={styles.logo} />
-      <h1 className={styles.heading}>Sign-in With Credentials</h1>
+      <h1 className={styles.heading}>Login With Credentials</h1>
     </div>
   )
 }
